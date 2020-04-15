0c3a0554661cc792881b394c3639e10683e772f8
Amos Wenger <amoswenger@gmail.com>
2020-04-15 20:15:54 +0200

    Working dynamic library setup

---
+++ libbutler/notes.md
@@ -0,0 +1,10 @@
+butler.def: manual
+
+For DLL, buildmode=c-shared, set output name
+
+generating butler.lib: from MSVC build tools:
+
+```shell
+lib /def:butler.def /out:butler.lib /machine:x64
+```
+
--- native/build.rs
+++ native/build.rs
@@ -7,41 +7,50 @@ fn main() {
 }
 
 fn build_libbutler() -> Result<(), Box<dyn Error>> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
-    let lib_dir = out_dir.join("libbutler-prefix");
+    println!("cargo:warning={}", "experimental libbutler build");
+
+    let pwd = env::current_dir().unwrap();
+    let lib_dir = pwd.parent().unwrap().join("libbutler");
+    println!("cargo:warning=libdir = {}", lib_dir.display());
+
     println!("cargo:rustc-link-lib={}", "butler");
     println!("cargo:rustc-link-search={}", lib_dir.display());
 
-    let lib_path = lib_dir.join("libbutler.a");
+    // let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    // let lib_dir = out_dir.join("libbutler-prefix");
+    // println!("cargo:rustc-link-lib={}", "butler");
+    // println!("cargo:rustc-link-search={}", lib_dir.display());
 
-    let dasho_arg = format!("-o={}", lib_path.display());
-    let mut cmd = Command::new("go");
-    cmd.args(&[
-        "build",
-        "-v",
-        "-buildmode=c-archive",
-        &dasho_arg,
-        "../libbutler",
-    ]);
-    let output = cmd.output()?;
-    if !output.status.success() {
-        panic!(
-            "Could not build libbutler (status {:?}):\n {}",
-            output.status,
-            std::str::from_utf8(&output.stderr)?
-        );
-    }
+    // let lib_path = lib_dir.join("libbutler.a");
 
-    let mut cmd = Command::new("ar");
-    cmd.args(&["-s", lib_path.to_str().unwrap()]);
-    let output = cmd.output()?;
-    if !output.status.success() {
-        panic!(
-            "Could not build libbutler (status {:?}):\n {}",
-            output.status,
-            std::str::from_utf8(&output.stderr).unwrap()
-        );
-    }
+    // let dasho_arg = format!("-o={}", lib_path.display());
+    // let mut cmd = Command::new("go");
+    // cmd.args(&[
+    //     "build",
+    //     "-v",
+    //     "-buildmode=c-archive",
+    //     &dasho_arg,
+    //     "../libbutler",
+    // ]);
+    // let output = cmd.output()?;
+    // if !output.status.success() {
+    //     panic!(
+    //         "Could not build libbutler (status {:?}):\n {}",
+    //         output.status,
+    //         std::str::from_utf8(&output.stderr)?
+    //     );
+    // }
+
+    // let mut cmd = Command::new("ar");
+    // cmd.args(&["-s", lib_path.to_str().unwrap()]);
+    // let output = cmd.output()?;
+    // if !output.status.success() {
+    //     panic!(
+    //         "Could not build libbutler (status {:?}):\n {}",
+    //         output.status,
+    //         std::str::from_utf8(&output.stderr).unwrap()
+    //     );
+    // }
 
     Ok(())
 }
