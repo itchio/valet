acb075f3efa3de653b6eae2500eda1e8d0a7e478
Amos Wenger <amoswenger@gmail.com>
2020-05-20 13:48:46 +0200

    Make 'version' script bump Cargo.toml

--- release/version.js
+++ release/version.js
@@ -3,11 +3,34 @@
 
 const { info, $ } = require("./common");
 const { generateTypings } = require("./generate-typings");
+const { readFileSync, writeFileSync } = require("fs");
 
 /**
  * @param {string[]} args
  */
 async function main(args) {
+  const { version } = require("../package.json");
+  info(`Bumped to version ${version}`);
+
+  info("Editing Cargo.toml...");
+  let contents = readFileSync("Cargo.toml", { encoding: "utf8" });
+  let lines = contents.split("\n");
+  let foundVersion = false;
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (/version = ".*"/.test(line)) {
+      lines[i] = `version = ${JSON.stringify(version)}`;
+      foundVersion = true;
+      break;
+    }
+  }
+  if (!foundVersion) {
+    throw new Error("Could not find version line in Cargo.toml!");
+  }
+
+  contents = lines.join("\n");
+  writeFileSync("Cargo.toml", contents, { encoding: "utf8" });
+
   info("Generating JSON-RPC typings...");
   generateTypings();
   info("Compiling TypeScript code...");
