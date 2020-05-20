d0ab0afff86407053758d172eca464eb4062262b
Amos Wenger <amoswenger@gmail.com>
2020-05-20 20:40:31 +0200

    also export namespaced..

--- ts/index.ts
+++ ts/index.ts
@@ -2,6 +2,7 @@ export * from "./support";
 export * from "./client";
 export * from "./conversation";
 export * from "./messages";
+export * as messages from "./messages";
 
 export interface InitOpts {
   /** path to DB file */
