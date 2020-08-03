068f52c9bcc18afc14e247cabc386d36d6af9c97
Amos Wenger <amoswenger@gmail.com>
2020-08-03 19:46:39 +0200

    Custom AsyncRead implementation, preparing for caching

--- htfs/src/lib.rs
+++ htfs/src/lib.rs
@@ -7,7 +7,14 @@ use futures::io::AsyncRead;
 use futures::prelude::*;
 use futures::stream::TryStreamExt;
 use reqwest::Method;
-use std::{collections::HashMap, fmt};
+use std::{
+    collections::HashMap,
+    fmt, io,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll},
+    time::Duration,
+};
 use url::Url;
 
 #[derive(Debug, thiserror::Error)]
@@ -81,13 +88,98 @@ impl File {
                     }
                 }
             };
+            let reader = Box::pin(stream).into_async_read();
+            let reader = Reader {
+                reader: Arc::new(Mutex::new(reader)),
+                fut: None,
+            };
 
-            Ok(Box::pin(stream).into_async_read())
+            Ok(reader)
         }
     }
 
     pub fn size(&self) -> u64 {
         self.size
+    }
+}
+
+struct Reader<'a, R>
+where
+    R: AsyncRead + Unpin + 'a,
+{
+    reader: Arc<Mutex<R>>,
+    fut: Option<Pin<Box<dyn Future<Output = io::Result<(Vec<u8>, usize)>> + 'a>>>,
+}
+
+impl<'a, R> Reader<'a, R>
+where
+    R: AsyncRead + Unpin + 'a,
+{
+    async fn private_read(
+        reader: Arc<Mutex<R>>,
+        mut buf: Vec<u8>,
+    ) -> std::io::Result<(Vec<u8>, usize)> {
+        tracing::info!("waiting...");
+        tokio::time::delay_for(Duration::from_millis(200)).await;
+        tracing::info!("reading!");
+
+        let mut reader = reader.lock().unwrap();
+        match reader.read(&mut buf).await {
+            Ok(n) => Ok((buf, n)),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl<'a, R> AsyncRead for Reader<'a, R>
+where
+    R: AsyncRead + Unpin + 'a,
+{
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut [u8],
+    ) -> Poll<std::io::Result<usize>> {
+        tracing::info!("reader::poll_read");
+
+        let mut fut = match self.fut.take() {
+            Some(fut) => {
+                tracing::info!("polling existing");
+                fut
+            }
+            None => {
+                tracing::info!("making new future");
+                let buf2 = vec![0u8; buf.len()];
+                Box::pin(Self::private_read(self.reader.clone(), buf2))
+            }
+        };
+        let res = fut.as_mut().poll(cx);
+        self.fut = Some(fut);
+        match res {
+            Poll::Ready(res) => match res {
+                Ok((buf2, n)) => {
+                    for i in 0..n {
+                        buf[i] = buf2[i]
+                    }
+                    Poll::Ready(Ok(n))
+                }
+                Err(e) => Poll::Ready(Err(e)),
+            },
+            Poll::Pending => Poll::Pending,
+        }
+    }
+    fn poll_read_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &mut [io::IoSliceMut<'_>],
+    ) -> Poll<io::Result<usize>> {
+        for b in bufs {
+            if !b.is_empty() {
+                return self.poll_read(cx, b);
+            }
+        }
+
+        self.poll_read(cx, &mut [])
     }
 }
 
